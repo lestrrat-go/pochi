ed39744fb3037d19de1d76d4096ef3614d72a4e8
Daisuke Maki <lestrrat+github@users.noreplay.github.com>
2024-10-18 07:52:51 +0900

    Make routing work even when we add an ancestor path after a leaf

--- pochi.go
+++ pochi.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"path"
 	"strings"
+	"sync"
 
 	"github.com/lestrrat-go/trie/v2"
 )
@@ -26,6 +27,7 @@ type TrieProvider interface {
 }
 
 type router struct {
+	mu          sync.RWMutex
 	paths       *pathtrie
 	cachedPaths map[string]*PathSpec
 }
@@ -98,11 +100,29 @@ func ErrInvalidPath() error {
 }
 
 func (r *router) Route(specs ...*PathSpec) error {
+	// perform checks _only_ first, so that we don't end up
+	// with some path objects already processed and some erroring out
 	for _, spec := range specs {
 		if !strings.HasPrefix(spec.pattern, "/") {
 			return fmt.Errorf("paths must be absolute %q: %w", spec.pattern, ErrInvalidPath())
 		}
 
+		// If for whatever reason this path object is already compiled,
+		// we refuse to proceed
+		if spec.compiled.Load() { // TODO: crete proper interface
+			return fmt.Errorf("path %q is already compiled", spec.pattern)
+		}
+	}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.nlRoute(true, specs...)
+}
+
+func (r *router) nlRoute(recurse bool, specs ...*PathSpec) error {
+	paths := make([]string, 0, len(specs))
+	for _, spec := range specs {
+		paths = append(paths, spec.pattern)
 		r.paths.Put(spec.pattern, spec)
 		node, ok := r.paths.GetNode(spec.pattern)
 		if !ok {
@@ -122,10 +142,41 @@ func (r *router) Route(specs ...*PathSpec) error {
 			}
 		}
 	}
+
+	if !recurse {
+		return nil
+	}
+
+	// It is possible to add /foo/ after /foo/bar, so we need to clear the cache, and
+	// re-evaluate the paths
+	var reroute []*PathSpec
+	Walk(r, RouteVisitFunc(func(fullpath string, spec *PathSpec) bool {
+		for _, p := range paths {
+			if fullpath != p && strings.HasPrefix(fullpath, p) {
+				delete(r.cachedPaths, fullpath)
+				spec.invalidate()
+				// r.paths.Delete(spec.pattern)
+				reroute = append(reroute, spec)
+				break
+			}
+		}
+		return true
+	}))
+
+	if err := r.nlRoute(false, reroute...); err != nil {
+		return fmt.Errorf(`failed to re-route after adding new paths: %w`, err)
+	}
+
 	return nil
 }
 
 func (r *router) MatchRoute(p string) (*PathSpec, bool) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.nlMatchRoute(p)
+}
+
+func (r *router) nlMatchRoute(p string) (*PathSpec, bool) {
 	for p != "" {
 		spec, ok := r.paths.Get(p)
 		if !ok && p != "/" {
@@ -150,23 +201,28 @@ func (r *router) MatchRoute(p string) (*PathSpec, bool) {
 }
 
 func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	r.mu.Lock()
 	pathkey := req.URL.Path
 	spec, ok := r.cachedPaths[pathkey]
 	if !ok {
-		spec, ok = r.MatchRoute(pathkey)
+		spec, ok = r.nlMatchRoute(pathkey)
 		if !ok || !spec.HasHandler() {
+			r.mu.Unlock()
 			http.NotFound(w, req)
 			return
 		}
+		r.cachedPaths[pathkey] = spec
 	}
-	r.cachedPaths[pathkey] = spec
+	r.mu.Unlock()
 	spec.ServeHTTP(w, req)
 }
 
 func (r *router) Mount(prefix string, router Router) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
 	return Walk(router, RouteVisitFunc(func(fullpath string, spec *PathSpec) bool {
 		p := MountPath(prefix, spec)
-		if err := r.Route(p); err != nil {
+		if err := r.nlRoute(true, p); err != nil {
 			return false
 		}
 		return true
